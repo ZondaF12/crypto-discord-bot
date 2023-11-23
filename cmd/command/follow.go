ce278aa2d64c0d817776cd9478022fd4c115cb3e
zondaf12 <ruaridhbell73@googlemail.com>
2023-11-23 23:45:50 +0000

    Fixed response string not having coin in caps

--- cmd/command/follow.go
+++ cmd/command/follow.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/ZondaF12/crypto-bot/cmd/database"
 	"github.com/bwmarrin/discordgo"
@@ -16,7 +17,7 @@ func FollowCrypto(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		fmt.Println(err)
 	}
 
-	content := fmt.Sprintf("Price Alert for %s scheduled in %s", options[0].StringValue(), options[1].StringValue())
+	content := fmt.Sprintf("Price Alert for %s scheduled in %s", strings.ToUpper(options[0].StringValue()), options[1].StringValue())
 
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		// Ignore type for now, they will be discussed in "responses"
